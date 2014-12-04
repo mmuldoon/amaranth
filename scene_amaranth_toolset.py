149a5486f3d6aee71612e1d7fd5e52cef51a6664
Matthew Muldoon <matthew@blendswap.com>
2014-12-04 14:33:45 -0600

    Added Bswap Admin Tools
    Dissected Amaranth scene debug code to start working on the base of
    Bswap Admin Tools

--- scene_amaranth_toolset.py
+++ scene_amaranth_toolset.py
@@ -45,8 +45,7 @@ cycles_exists = False
 
 def check_cycles_exists():
     global cycles_exists
-    cycles_exists = ('cycles' in dir(bpy.types.Scene))
-    return cycles_exists
+    cycles_exists = ('cycles' in dir(bpy.types.Scene))    return cycles_exists
 
 
 check_cycles_exists()
